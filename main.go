1374f7391af83275d2d923cd73d7bf421b031af6
linda2927 <linda2927@naver.com>
2023-03-31 20:22:24 +0900

    declaring and using functions

--- main.go
+++ main.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/EarthlyZ9/learngo/somefunc"
 )
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+// Go function can return multiple values
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+// initalizing variables when declaring return types
+func lenAndUpper2(name string) (length int, uppercase string) {
+	// do something after return
+	defer fmt.Println("done!")
+
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+
+	// return length, uppercase
+	return // naked return
+}
+
+// multiple arguments
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
 	fmt.Println("Hello world!")
@@ -24,5 +51,16 @@ func main() {
 	short_name := "jisoo" // Go guesses the type automatically
 	short_name = "js"
 	fmt.Println(short_name)
+
+	// using functions
+	fmt.Println(multiply(2, 2))
+	totalLength, upperName := lenAndUpper("jisoo")
+	// totalLength, _ := lenAndUpper("jisoo")
+
+	fmt.Println(totalLength, upperName)
+
+	repeatMe("js", "earthlyz9", "jisoo", "leejisoo") // returns array
+
+	fmt.Println(lenAndUpper2("jisoo"))
 
 }
